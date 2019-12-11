77a932c13fc3f51b23f7f34263298daaa9a21687
esjo <esjo@itu.dk>
2019-12-11 20:41:08 +0100

    addad raise_temp()

--- hottub.py
+++ hottub.py
@@ -3,15 +3,32 @@ from time import sleep
 import temp_sensor
 
 
-interval = 10 #interval between tests (sec)
-sensor = temp_sensor
-min_temp = 25 #celcius
+interval = 10                 #interval between tests (sec)
+min_temp = 25                 #celcius
+standby_temp = min_temp + 5   #celcius
 
-while True:
-    if(sensor.current_temperature() < min_temp):
-        print("TO COLD TRIGGER RELAY: " + str(datetime.now().strftime("%H:%M:%S")))
 
-    else:
-        print("temp fine")
-    print(str(temp_sensor.current_temperature()))
-    sleep(interval)
+def raise_temp(goal):
+    while True:
+        curr = temp_sensor.current_temperature()
+        if(curr >= goal):
+            print("GOAL REACHED!")
+            #do relay stuff!
+            return
+        sleep(interval-1) 
+
+
+def standby_mode():
+    while True:
+        if(sensor.current_temperature() < min_temp):
+            print("TO COLD TRIGGER RELAY: " + str(datetime.now().strftime("%H:%M:%S")))
+            raise_temp(standby_temp)
+        else:
+            print("temp fine")
+        print(str(temp_sensor.current_temperature()))
+        sleep(interval)
+
+
+
+#this is the start!
+standby_mode()
