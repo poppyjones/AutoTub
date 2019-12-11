f1aa5474820da7f804292e6adefee569cc4e3604
esjo <esjo@itu.dk>
2019-12-11 20:24:28 +0100

    basic standby - no relay

--- hottub.py
+++ hottub.py
@@ -1,4 +1,16 @@
 from datetime import datetime
+from time import sleep
 import temp_sensor
 
-print(str(temp_sensor.current_temperature()))
+
+interval = 10 #interval between tests (sec)
+sensor = temp_sensor
+min_temp = 25 #celcius
+
+while True:
+    if(sensor.current_temperature < min_temp):
+        print("TO COLD TRIGGER RELAY: {%H:%M:%S}".format(datetime.now()))
+    else:
+        print("temp fine")
+    sleep(interval)
+    print(str(temp_sensor.current_temperature()))
