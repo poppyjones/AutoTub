c0c8c8a6767632af75e57257158d5ee8d1def8a5
poppyjones <poppy.em.jones@gmail.com>
2019-12-11 17:49:41 +0000

    added import statements for sensor

--- testscripts/hellotemp.py
+++ testscripts/hellotemp.py
@@ -1,7 +1,14 @@
 from datetime import datetime
+import board
+import busio
+import digitalio
+import adafruit_max31865
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+cs = digitalio.DigitalInOut(board.D5)  # Chip select of the MAX31865 board.
+sensor = adafruit_max31865.MAX31865(spi, cs, wires=3)
 
 f = open("tempLog.txt", "a")
 f.write("Hello! This script was called: ")
 f.write(str(datetime.now().strftime("%d %b %Y %H:%M:%S")) + "\n")
-f.write("   The temprature was: {0:0.3f}C\n".format(sensor.temprature))
+f.write("   The temprature was: {0:0.3f}C\n".format(sensor.temperature))
 f.close()
