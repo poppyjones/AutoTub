837fb0736717b9e03893eda23ce179bb090d0a72
poppyjones <poppy.em.jones@gmail.com>
2019-12-13 12:00:21 +0100

    deletede duplicates

--- callexception.py
+++
@@ -1,6 +0,0 @@
-import exceptiontest
-
-try:
-    exceptiontest.run()
-except Exception as err:
-    print(err)
--- exceptiontest.py
+++
@@ -1,5 +0,0 @@
-from time import sleep
-
-def run():
-    sleep(5)
-    raise Exception("Hardware down???")
