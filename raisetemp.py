38018b9f8ff7da250367807385dfeb0e7e10e396
poppyjones <poppy.em.jones@gmail.com>
2020-01-13 21:47:00 +0100

    quick fix

--- raisetemp.py
+++ raisetemp.py
@@ -3,6 +3,6 @@ import hottub
 print("How warm do you want the hottub to be?")
 try:
     t = float(input())
-    hottub.maintain_temp(t,t+2)
+    hottub.maintain_temp(t-1,t)
 except Exception as err:
     print("That's not a number - try again!")
