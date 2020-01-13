9871fc642aed375f5bb50d324ae15f9da496aee0
poppyjones <poppy.em.jones@gmail.com>
2020-01-13 21:26:45 +0100

    fixed type

--- raisetemp.py
+++ raisetemp.py
@@ -2,7 +2,7 @@ from hottub import raise_temp
 
 print("How warm do you want the hottub to be?")
 try:
-    t = double(input())
+    t = float(input())
     raise_temp(t)
 except Exception as err:
     print("That's not a number - try again!")
